232ddf86a2f178442154788c4a98e277aba490a2
markku <markku@markku-VirtualBox.(none)>
2015-03-22 22:50:18 +0200

    korjailua2

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,27 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+//   	  View::make('home.html');
+        echo 'Tämä on etusivu!';
+        
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
     }
+    
+    public static function etusivu(){
+        View::make('suunnitelmat/etusivu.html');
+    }
+    
+    public static function esittely_julkinen(){
+        View::make('suunnitelmat/esittely_julkinen.html');
+    }
+    
+    public static function profiilin_muokkaus(){
+        View::make('suunnitelmat/profiilin_muokkaus.html');
+    }
+    
+    
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,15 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/etusivu', function() {
+    HelloWorldController::etusivu();
+  });
+  
+  $routes->get('/esittely_julkinen', function() {
+    HelloWorldController::esittely_julkinen();
+  });
+  
+  $routes->get('/profiilin_muokkaus', function() {
+    HelloWorldController::profiilin_muokkaus();
+  });
