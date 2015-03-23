fde70192e36aef4e5ed8952c01835c6c58360201
markku <markku@markku-VirtualBox.(none)>
2015-03-23 04:19:15 +0200

    taulujen luontilauseet

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,16 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Asiakas (etunimi, sukunimi, nimimerkki, kayttajatunnus, salasana, syntymaaika, sukupuoli, katuosoite, postinumero, paikkakunta, haettu_ika_min, haettu_ika_max, haettu_sukupuoli, esittelyteksti) VALUES ('Antti','Virtanen','Pelimies','av_dude','123456','1988-05-12','M','Hämeentie 78 B 12','00260','Helsinki',20,30,'F','Olen iloinen ja reipas tietojenkäsittelytieteen opiskelija. Pidän kovasti tietokonepeleistä ja energiajuomista.');
+
+INSERT INTO Asiakas (etunimi, sukunimi, nimimerkki, kayttajatunnus, salasana, syntymaaika, sukupuoli, katuosoite, postinumero, paikkakunta, haettu_ika_min, haettu_ika_max, haettu_sukupuoli, esittelyteksti) VALUES ('Minttu','Korhonen','Barbie','bb-mint','123456','1990-04-24','F','Helsinginkatu 21 C 5','00240','Helsinki',20,30,'M','Olen jazz-tanssia ja kuorolaulua harrastava matemaatikko.');
+
+INSERT INTO Viesti (lahettaja, vastaanottaja, sisalto) VALUES (1,2,'Hei, haluaisitko lähetä keskiviikkoiltana kahville?');
+INSERT INTO Viesti (lahettaja, vastaanottaja, sisalto) VALUES (2,1,'Hei, keskiviikko ei käy, miten olis torstai?');
+
+INSERT INTO Esittelysivu (asiakasID, sisalto, salainen) VALUES (1,'En ole vielä keksinyt tähän mitään...', true);
+INSERT INTO Esittelysivu (asiakasID, sisalto, salainen) VALUES (2,'Salainen paheeni on kettukarkit!',false);
+
+INSERT INTO Esittelysivu (asiakasID, sisalto, salainen) VALUES (1, 'En tykkää yhtään mämmistä.',true);
+
+INSERT INTO Lukuoikeus (sivuID, asiakasID) VALUES (3,2);
+
+INSERT INTO Yllapitaja (kayttajatunnus, salasana) VALUES ('admin','anasalas');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,44 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Asiakas(
+	asiakasID SERIAL PRIMARY KEY,
+	etunimi varchar(15) NOT NULL,
+	sukunimi varchar(25) NOT NULL,
+	nimimerkki varchar(20) NOT NULL,
+	kayttajatunnus varchar(20) UNIQUE NOT NULL,
+	salasana varchar(20) NOT NULL,
+	syntymaaika DATE NOT NULL,
+	sukupuoli char NOT NULL,
+	katuosoite varchar(30) NOT NULL,
+	postinumero varchar(6) NOT NULL,
+	paikkakunta varchar(25) NOT NULL,
+	haettu_ika_min integer,
+	haettu_ika_max integer,
+	haettu_sukupuoli char,
+	esittelyteksti varchar(2000)
+);
+
+CREATE TABLE Esittelysivu(
+	sivuID SERIAL PRIMARY KEY,
+	asiakasID integer REFERENCES Asiakas (asiakasID),
+	sisalto varchar(2000),
+	salainen boolean
+);
+
+CREATE TABLE Viesti(
+	viestiID SERIAL PRIMARY KEY,
+	lahettaja integer REFERENCES Asiakas (asiakasID),
+	vastaanottaja integer REFERENCES Asiakas (asiakasID),
+	sisalto varchar(2000)
+);
+
+CREATE TABLE Lukuoikeus(
+	sivuID integer REFERENCES Esittelysivu (sivuID),
+	asiakasID integer REFERENCES Asiakas (AsiakasID),
+	PRIMARY KEY (sivuID, asiakasID)
+);
+
+CREATE TABLE Yllapitaja(
+	yllapitajaID SERIAL PRIMARY KEY,
+	kayttajatunnus varchar(20) UNIQUE NOT NULL,
+	salasana varchar(20) NOT NULL
+);
+	
